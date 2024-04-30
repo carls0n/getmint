cee68291a652b860f885e6a3a1b2f22fb1030af1
Marc Carlson <138807964+carls0n@users.noreply.github.com>
2024-04-30 13:57:45 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+# 
+* Check for and download latest Linux Mint installation iso<br>
+* Optionally, download any other available installation iso<br>
+* Can be used with any systemd based Linux distrobution<br>
+* Choose your preferred edition, XFCE, Mate, Cinnamon or Edge.<br>
+* Edit script to choose your default edition and mirror for a simple download.<br>
+* Supports the use of mirrors
+#
---
+++ getmint.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+
+# getmint.sh - Marc Carlson 2024
+# My other repositories: https://github.com/carls0n/
+
+edition="xfce" # default edition
+mirror="mirrors.kernel.org" # default mirror
+
+function usage {
+echo "-v  (version) - Check for version other than latest version"
+echo "-e  edition (mate, xfce, cinnamon and edge"
+echo "-d  Download selected version"
+echo "-m  Use mirror instead. i.e, mirrors.giganet.com"
+}
+
+function get_args {
+   while getopts ":hv:de:om:" arg; do
+   case $arg in
+   v) version="$OPTARG" ;;
+   d) download=1;;
+   h) usage && exit;;  
+   e) edition="$OPTARG";;
+   o) override=1;;
+   m) mirrors="$OPTARG";;
+   esac
+   done
+}
+
+function systemd_check {
+[[ ! -d /run/systemd/system ]] && echo This script requires a systemd based Linux system. && exit
+}
+
+function warn {
+if [[ ! $override ]] &&  [[ $(hostnamectl | grep Op* | awk '{print $3,$4}') != "Linux Mint" ]]
+then 
+echo "This script is meant to be used with a Linux Mint installation. 
+use -o to override and indicate which version and edition you want to download."
+exit
+fi
+}
+
+installed=$(hostnamectl | grep Op* | awk '{print $5}')
+if [[ $installed == *.3 ]]; then
+version=$(bc <<< "$installed+0.7")
+else
+version=$(bc <<< "$installed+0.1")
+fi
+
+function test {
+if [[ $download == "1" ]] && [[ $edition == "edge" ]]
+then 
+wget -q -c --show-progress https://$mirror/linuxmint/stable/$version/linuxmint-$version-cinnamon-64bit-edge.iso
+
+elif [[ $download == "1" ]] && [[ $edition != "edge" ]]; then
+wget -q -c --show-progress https://$mirror/linuxmint/stable/$version/linuxmint-$version-$edition-64bit.iso
+exit
+fi
+
+response=$(curl -L -so /dev/null -w '%{http_code}\n' https://$mirror/linuxmint/stable/$version/)
+if [[ $response == "404" ]]
+then printf "Linux Mint version $version $edition edition not yet released.\n"
+
+elif [[ $response == "200" ]]
+then printf "Linux Mint version $version $edition edition is available for download.\n"
+fi
+}
+
+get_args $@
+systemd_check
+warn
+test
